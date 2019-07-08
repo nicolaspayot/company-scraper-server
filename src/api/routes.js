0326e0f84a4174e8aba627c6378ae18df16dadf2
Nicolas Payot <nicolas.payot@gmail.com>
2019-07-08 17:55:14 +0200

    test(api): validate POST requests to /companies/routes

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,10 +1,22 @@
+const Joi = require('@hapi/joi');
+
 module.exports = {
   name: 'routes',
   register: async server => {
     server.route({
-      method: 'GET',
-      path: '/',
-      handler: () => 'Hello World!',
+      method: 'POST',
+      path: '/companies/routes',
+      handler: () => 'Hello, World!',
+      options: {
+        validate: {
+          payload: Joi.object()
+            .keys({
+              linkedin: Joi.string().uri(),
+              societe: Joi.string().uri(),
+            })
+            .or('linkedin', 'societe'),
+        },
+      },
     });
   },
 };
---
+++ src/api/routes.spec.js
@@ -0,0 +1,41 @@
+const Hapi = require('@hapi/hapi');
+const api = require('./index');
+
+const requestWithPayload = payload => {
+  return {
+    method: 'POST',
+    url: '/api/companies/routes',
+    payload,
+  };
+};
+
+describe('POST - /api/companies/routes', () => {
+  let server;
+
+  beforeEach(async () => {
+    server = new Hapi.Server();
+    await api(server);
+  });
+
+  it('should return 400 error code if request has no payload', async () => {
+    const request = requestWithPayload();
+    const response = await server.inject(request);
+    expect(response.statusCode).toBe(400);
+  });
+
+  it('should return 400 error code if request has empty payload', async () => {
+    const request = requestWithPayload({});
+    const response = await server.inject(request);
+    expect(response.statusCode).toBe(400);
+  });
+
+  it('should return 400 error code if request has invalid payload', async () => {
+    const request1 = requestWithPayload({ linkedin: '', societe: '' });
+    const response1 = await server.inject(request1);
+    expect(response1.statusCode).toBe(400);
+
+    const request2 = requestWithPayload({ linkedin: 'NOT_A_URI', societe: 'NOT_A_URI' });
+    const response2 = await server.inject(request2);
+    expect(response2.statusCode).toBe(400);
+  });
+});
