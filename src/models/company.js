cf105fb8b32dc0a9953e52d690b5fb6ed817d9c9
Nicolas Payot <nicolas.payot@gmail.com>
2019-07-09 11:26:43 +0200

    feat(models): add Company mongoose model

---
+++ src/models/company.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Company = new mongoose.Schema(
+  {
+    linkedinURL: {
+      type: String,
+      index: true,
+    },
+    societeURL: {
+      type: String,
+      index: true,
+    },
+    logoURL: String,
+    name: String,
+    publicName: String,
+    industry: String,
+    address: String,
+    siren: String,
+    siret: String,
+    employees: Number,
+    employeesOnLinkedin: Number,
+    shareCapital: String,
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model('Company', Company);
