e4d6cbd91ddf0f47eb76d0a07fd0dd07e0483483
Nicolas Payot <nicolas.payot@gmail.com>
2019-07-18 22:29:57 +0200

    feat(api): add hapi-cors for CORS

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,17 @@
 const config = require('../config');
 const routes = require('./routes');
 
-module.exports = server =>
-  server.register(routes, {
+module.exports = async server => {
+  await server.register({
+    plugin: require('hapi-cors'),
+    options: {
+      origins: ['http://localhost:8080'],
+    },
+  });
+
+  await server.register(routes, {
     routes: {
       prefix: config.api.prefix,
     },
   });
+};
